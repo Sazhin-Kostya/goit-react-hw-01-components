e4735355906e5fd6352026f01386bf747364c05a
Sazhin-Kostya <sazhinkostya@gmail.com>
2023-08-20 18:25:01 +0300

    test

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,42 @@
+import user from './user.json';
+
+// {
+//   /* <Profile
+//   username={user.username}
+//   tag={user.tag}
+//   location={user.location}
+//   avatar={user.avatar}
+//   stats={user.stats}
+// />; */
+// }
+
+export default function Profile(params) {
+  return (
+    <>
+      <div class="profile">
+        <div class="description">
+          <img src={user.avatar} alt="User avatar" class="avatar" />
+          <p class="name">{user.username}</p>
+          <p class="tag">{user.tag}</p>
+          <p class="location">{user.location}</p>
+        </div>
+
+        <ul class="stats">
+          <li>
+            <span class="label">Followers</span>
+            <span class="quantity">{user.stats.followers}</span>
+          </li>
+          <li>
+            <span class="label">Views</span>
+            <span class="quantity">{user.stats.views}</span>
+          </li>
+          <li>
+            <span class="label">Likes</span>
+            <span class="quantity">{user.stats.likes}</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+console.log(Profile);
